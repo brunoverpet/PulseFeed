7b4f5684dee2d33c0fb0b851ace340ae683fa2a0
Bruno Verpet <bverpet46@gmail.com>
2025-10-03 22:40:07 +0200

    remove controller test

--- server/app/controllers/tests_controller.ts
+++
@@ -1,9 +0,0 @@
-import type { HttpContext } from '@adonisjs/core/http'
-
-export default class TestsController {
-  async index({ response }: HttpContext) {
-    return response.ok({
-      hello: 'Bruno' as const,
-    })
-  }
-}

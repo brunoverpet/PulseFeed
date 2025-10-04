3fddbb2ca11d68671291cd754d9674dd12c0d647
Bruno Verpet <bverpet46@gmail.com>
2025-10-04 19:47:17 +0200

    chore: install base template for editor setup

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,17 +1,27 @@
 import { tuyau } from '@/app/utils/tuyau'
+import { SimpleEditor } from '@/components/tiptap-templates/simple/simple-editor'
 
 export default async function Home() {
   const message = await tuyau.articles.$get()
 
-  if (!message.data || !message.data.articles.length) {
-    return <div>No articles found</div>
-  }
+  // if (!message.data || !message.data.articles.length) {
+  //   return <div>No articles found</div>
+  // }
 
   return (
     <div>
-      {message.data.articles.map((article) => (
-        <h1 key={article.id}>{article.data.title}</h1>
-      ))}
+      {/*<SimpleEditor />*/}
+      <SimpleEditor
+      // onSave={async ({ json, html }) => {
+      //   await tuyau.articles.$post({
+      //     body: { contentJson: json, contentHtml: html },
+      //   })
+      // }}
+      />
+
+      {/*{message.data.articles.map((article) => (*/}
+      {/*  <h1 key={article.id}>{article.data.title}</h1>*/}
+      {/*))}*/}
     </div>
   )
 }

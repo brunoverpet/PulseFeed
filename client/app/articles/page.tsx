22f77fb2e31a49f70d5570204f419aadf4c10285
Bruno Verpet <bverpet46@gmail.com>
2025-10-07 16:27:33 +0200

    Feature: improve article fetching and list rendering by slug

--- client/app/articles/page.tsx
+++ client/app/articles/page.tsx
@@ -1,8 +1,26 @@
-export default function Articles() {
+import { tuyau } from '@/app/utils/tuyau'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+
+export default async function Articles() {
+  const articles = await tuyau.articles.$get()
+  const slug = 'slug-article-2'
+  const article = await tuyau.articles({ slug }).$get()
+
+  // console.log(articles)
+  console.log(article)
   return (
-    <div>
-      <h1>Articles</h1>
-      <p>This is the articles page.</p>
+    <div className="mx-auto p-4">
+      <h1 className="text-2xl mb-4">Articles</h1>
+      {articles.data.articles.map((article) => (
+        <div key={article.id} className="flex flex-col gap-2">
+          <h2>{article.title}</h2>
+          <p>{article.metaDescription}</p>
+          <Link href={`/articles/${article.slug}`}>
+            <Button>Voir l'article</Button>
+          </Link>
+        </div>
+      ))}
     </div>
   )
 }
